abc642a1613b6abe179025c145b798f671fe3312
Sergi Quero Invernon <sergiquero@gmail.com>
2025-09-05 19:24:01 +0200

    Create blueprint logbookviewmodel

---
+++ app/src/main/java/com/example/appointmentlistapp/viewmodels/LogBookViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.appointmentlistapp.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.appointmentlistapp.data.LogBook
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class LogBookViewModel {
+
+
+
+}
+
