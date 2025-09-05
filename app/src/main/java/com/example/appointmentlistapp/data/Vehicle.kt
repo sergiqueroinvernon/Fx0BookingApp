05d4a9637c76e67797f52cb9e3cac0a0f03b9675
Sergi Quero Invernon <sergiquero@gmail.com>
2025-09-05 18:37:19 +0200

    Change properties of vehicle

--- app/src/main/java/com/example/appointmentlistapp/data/Vehicle.kt
+++ app/src/main/java/com/example/appointmentlistapp/data/Vehicle.kt
@@ -1,8 +1,6 @@
 package com.example.appointmentlistapp.data
 
 data class Vehicle(
-    val id: String,
-    val name: String,
-    val surname: String,
-    val email: String
+    val licensePlate: String,
+    val internNumber: String,
 )
