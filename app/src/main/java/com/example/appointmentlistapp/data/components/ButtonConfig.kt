660ec2ed75fbf588f606a5ceacceb31e7347b7f6
Sergi Quero Invernon <sergiquero@gmail.com>
2025-09-18 13:32:35 +0200

    Update the Button Config

---
+++ app/src/main/java/com/example/appointmentlistapp/data/components/ButtonConfig.kt
@@ -0,0 +1,10 @@
+package com.example.appointmentlistapp.data.components
+
+data class ButtonConfig(
+    val id: String,
+    val text: String,
+    val isVisible: Boolean,
+    val clientId: String,
+    val screenId: String
+
+)
