3d723016cf2b6fe96912d4d9b2966454f95c41e1
Sergi Quero Invernon <sergiquero@gmail.com>
2025-09-18 16:06:28 +0200

    Add button configurations to BookingRepository
    This commit adds a new function `getButtonsForClientsAndScreen` to the `BookingRepository` class.
    This function retrieves button configurations from the `BookingDao` for a specific client and screen, returning them as a `Flow` of `List<ButtonConfig>`.

--- app/src/main/java/com/example/appointmentlistapp/data/BookingRepository.kt
+++ app/src/main/java/com/example/appointmentlistapp/data/BookingRepository.kt
@@ -1,9 +1,26 @@
 package com.example.appointmentlistapp.data
 
+import com.example.appointmentlistapp.data.components.ButtonConfig
+import kotlinx.coroutines.flow.Flow
+
 //Repository -> Provide/Transform the data to the ViewModel. Intermediary between UI and Data Sources./Interact with the Networking Service
 class BookingRepository(private val bookingDao: BookingDao) {
+    /*Retrieves the button configurations for a specific client and screen
+    *
+    * This method delegates the data request to the BookingDao. In a more complex, this method could also contain login to:
+    *  1-Check a local cache or a remote API FIRST
+    * 2. Handle network requests and save the results to the database
+    * 3. Handle data conflicts or syncronization
+    *
+    * The Flow return type ensures that the UI will get real-time updates whenever the underlying database data changes
+    *@param clientId the unique ID of the client
+    * @param screenId the unique ID of the screen
+    * @return A flow of a List of ButtonConfig objects
+    * */
 
-
+    fun getButtonsForClientsAndScreen(clientId: String, screenId: String): Flow<List<ButtonConfig>>{
+        return bookingDao.getButtonsForClientAndScreen(clientId, screenId)
+    }
 
 
 
