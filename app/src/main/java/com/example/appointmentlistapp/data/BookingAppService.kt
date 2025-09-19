19447167add831a6604800391718955b16090e58
Sergi Quero Invernon <sergiquero@gmail.com>
2025-09-19 17:34:47 +0200

    Add BookingAppService and BookingApiModel
    This commit introduces two new files:
    
    - `BookingAppService.kt`: Defines an interface `BookingAppService` with a suspend function `getAppointments()` to fetch a list of `Booking` objects from the "api/appointments" endpoint.
    - `BookingApiModel.kt`: Defines a data class `BookingApiModel` to represent the structure of booking data retrieved from the API. It includes fields such as `id`, `driverId`, `driver`, `appointmentDateTime`, `status`, `description`, `createdAt`, and `updatedAt`.

--- app/src/main/java/com/example/appointmentlistapp/data/BookingAppService.kt
+++ app/src/main/java/com/example/appointmentlistapp/data/BookingAppService.kt
@@ -1,7 +1,25 @@
 package com.example.appointmentlistapp.data
 
+import androidx.camera.core.ImageProcessor
+import com.example.appointmentlistapp.data.api.BookingApiModel
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.PUT
+
 interface BookingAppService {
 
-    @Get("api/appointments")
+    @GET("api/appointments")
     suspend fun getAppointments(): List<Booking>
+    @GET("api/appointments/driver/{driverId}")
+    suspend fun getAppointmentsByDriverId(@Path("driverId") driverId: String): List<Booking>
+    @POST("api/appointments/{id}/checkin")
+    suspend fun checkInAppointment(@Path("id") id: String): Response<Void>
+    @POST("api/appointments")
+    suspend fun createAppointment(@Body appointment: BookingApiModel): BookingApiModel
+    @PUT("api/appointments/{id}")
+    suspend fun updateAppointment(@Path("id") id: String, @Body appointment: BookingApiModel): BookingApiModel
+
 }
