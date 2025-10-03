db666864e1b4988928e87037968412f32d7d1b6f
Sergi Quero Invernon <sergiquero@gmail.com>
2025-10-03 15:56:19 +0200

    Fix: Make `formatDate` return type nullable
    This commit updates the `formatDate` utility function to return a nullable `String?` instead of a non-nullable `String`. This change aligns the function's signature with its behavior, as it now correctly returns `null` when date parsing fails, preventing potential `NullPointerExceptions`.

--- app/src/main/java/com/example/appointmentlistapp/ui/screens/MainAppScreen.kt
+++ app/src/main/java/com/example/appointmentlistapp/ui/screens/MainAppScreen.kt
@@ -78,10 +78,10 @@ fun MainAppScreen(viewModel: AppointmentViewModel) {
             }
         }
     }
+
 }
 
-
-fun formatDate(dateString: String?): String {
+fun formatDate(dateString: String?): String? {
     return try {
         val parser = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault())
         val formatter = SimpleDateFormat("dd MMM yyyy, hh:mm a", Locale.getDefault())
